6d5ce283cdd7f1769a04f52b2a2098b9b25cf4bd
pranjaldatta <pranjaldatta99@gmail.com>
2020-01-11 02:46:23 +0530

    added battery

---
+++ src/modules/battery.py
@@ -0,0 +1,25 @@
+import os
+
+class battery:
+    """
+
+    A module that shows battery related information
+
+    """
+    def __init__(self, cli_api):
+        self.cli_api = cli_api
+
+    def main(self):    
+        cmd = "upower -i /org/freedesktop/UPower/devices/battery_BAT0 "+ \
+              "| grep \"vendor\|power supply\|updated\|state\|voltage\|"+ \
+              "percentage\|time to full\" $1"
+        var = os.popen(cmd).read()
+        response = ""
+        for line in var.splitlines():
+            response += line.strip() + "\n"
+        self.cli_api.show(text=response)
+        
+
+def eval(cli_api, params):
+    bat = battery(cli_api)
+    bat.main()
