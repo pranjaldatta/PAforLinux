2a8bb9f065579f1e1f0bb737e3c6e106acd0b252
pranjaldatta <pranjaldatta99@gmail.com>
2020-01-27 21:48:58 +0530

    added battery task

--- src/modules/battery.py
+++ src/modules/battery.py
@@ -17,8 +17,19 @@ class battery:
         response = ""
         for line in var.splitlines():
             response += line.strip() + "\n"
-        self.cli_api.show(text=response)
-        
+    
+        if self.cli_api.speak == True:
+            cmd = "upower -i /org/freedesktop/UPower/devices/battery_BAT0 "+ \
+                "| grep \"power supply\|state\|percentage\|time to full\" $1"          
+            var = os.popen(cmd).read()
+            sepakResponse = ""
+            for line in var.splitlines():
+                sepakResponse += line.strip() + "\n"            
+            
+            self.cli_api.show(text=response, speakOnly=sepakResponse)
+        else:
+            self.cli_api.show(text=response)    
+
 
 def eval(cli_api, params):
     bat = battery(cli_api)
