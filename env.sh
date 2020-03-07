1c3c18e1618cd5133d6b264c4cc25f2768bfd250
pranjaldatta <pranjaldatta99@gmail.com>
2020-03-07 18:20:08 +0530

    modified env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+#create conda env
 conda env create -f env.yaml
 eval "$(conda shell.bash hook)"
 conda activate LinuxPA
-pip install pyttsx3==2.71
+
+#create symlink at /usr/local/bin of Assistant
 BASEDIR=$(readlink -f "$0")
 BASEDIR=$(dirname $BASEDIR)
 sudo ln -s $BASEDIR/Assistant /usr/local/bin
 chmod 777 /usr/local/bin/Assistant
+
+#installing extra dependencies
+pip install pyttsx3==2.71
+sudo /src/modules/notes/install.sh i
+
+
